e2107b26ba37e2d9e3d503a0d497853dbd48c76e
guigeek38 <guigeek.b@live.fr>
2016-04-04 14:55:59 +0200

    Ajout commentaires

--- Exercice1.m
+++ Exercice1.m
@@ -8,7 +8,7 @@ close all;
 clc;
 
 %% Question 1 : Simulation des arrivées
-duree = 60;
+duree = 60; %Durée de la simulation
 
 T = poisson(duree);     %On génére les arrivées
 N = arrived(T, duree);  %On compte les arrivées
@@ -21,9 +21,9 @@ ylabel('N(t)');
 
 
 %% Question 2
-nbValeur = 50;
+nbValeur = 50; %Nombre de chaque simulation à faire pour l'estimation
+temps = [10, 20, 40, 60, 80, 100]; %Durée des simulations
 
-temps = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 for i=1:1:max(size(temps))
     data(i) = estimation(temps(i), nbValeur);
 end
@@ -33,6 +33,19 @@ plot(temps , data);
 title('Nombre moyen d arrivee');
 xlabel('Duree max : T');
 ylabel('Moyenne des N(t)');     
+
+% Sur la figure 2, on peut voir qu'avec la simulation obtenu avec un grand
+% nombre d'essai, on obtient une droite parfaitement linéaire.
+% Si on relève plusieurs points :
+%       - N(100) = 300.2 soit presque 300
+%       - N(60) = 176.8 soit presque 180
+%       - N(10) = 31.24 soit presque 30
+% On remarque que le coeficiant directeur de cette droite est 3, ce qui
+% corrspond à la valeur de Lambda.
+% En effet, on a pu voir dans le cours que l'on peut estimer :
+%           Lambda = N(T) / T           soit N(T) = T * Lambda
+% Et donc la fonction N(T) doit bien être linéaire, de coeficiant Lambda,
+% la simulation correspond bien à la théorie !
         
 
 %% Question 3
--- estimation.m
+++ estimation.m
@@ -1,11 +1,12 @@
-%% Fonction permettant de calculer plusieurs fois les arrivées aléatoire afin de calculer leur moyenne
+%% Fonction permettant de généner plusieurs fois des arrivées aléatoire afin de calculer leur moyenne
 %Prend en entrée le nombre de valeur sur lequel il faut calculer la moyenne
 % et la durée de l'expérience.
 %Retourne le N(t) moyen de toutes ces expériences
 
-
 function N=estimation(duree, nbValeur)
     for i = 1:1:nbValeur
+        %On prend la taille du vecteur qui contient les temps d'arrivé, ce
+        % qui nous donne le nombre d'arrivée
         T(i) = length(poisson(duree));
     end
     N = mean(T);
